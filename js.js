240026790faaa6a5c1160c96fca3bae075fefc80
Cleiton Arcanjo <152240025+CleitonArcanj0@users.noreply.github.com>
2025-12-11 16:19:02 -0300

    refactor: melhorias no código JavaScript, CSS e HTML
    refactor: melhorias no código JavaScript, CSS e HTML
    
    - Ajustes nos estilos de validação
    - Correções de seletores e IDs
    - Melhoria no fluxo da função contador
    - Revisão da estrutura do HTML

--- js.js
+++ js.js
@@ -1,55 +1,48 @@
 const frm =  window.document.querySelector("form")
-const resp = window.document.querySelector("h3")
 
 function contador(campo){
     const senha = campo.value
-    window.document.getElementById("contador").innerText = senha.length;
-    if(senha.length == 15){
-        document.querySelector("#pContador").style.color = "red"
-    }
-}
-frm.addEventListener("submit", (e)=>{
-    e.preventDefault()  
-    const senha =  frm.inSenha.value;
     const temNum = senha.match(/[0-9]/g)
     const temMinus = senha.match(/[a-z]/g)
     const temMaius = senha.match(/[A-Z]/g)
     const temEspeciais = senha.match(/\W/g)
-    const tam = senha.length
-    const erros = []
-    if(tam < 8){
-        alert("A senha deve possui 8 a 15 caracteres.")
-        frm.inSenha.focus()
-        return
+    
 
+    window.document.getElementById("contador").innerText = senha.length;
+    
+    if(senha.length == 15){
+        document.querySelector("#pContador").style.color = "red"
     }
+
+    if(senha.length > 8){
+        document.getElementById("list_regra1").style.color = "#48f115f8"
+    } else{
+        document.getElementById("list_regra1").style.color = '#f33e08f8'
+    }
+
     if(temNum == null){
-        erros.push("A senha tem que ter no mínimo 1 número")
+        document.getElementById("list_regra2").style.color = '#f33e08f8'
+    }else{
+        document.getElementById("list_regra2").style.color = "#48f115f8"
+    }
+    
+    if(temMinus == null){
+        document.getElementById("list_regra3").style.color = '#f33e08f8'
+    }else{
+         document.getElementById("list_regra3").style.color = "#48f115f8"
     }
 
-    if(temMinus == null){
-        erros.push("A senha tem que ter no mínimo 1 letra minúscula")
-    }
 
     if(temMaius == null || temMaius.length < 2){
-        erros.push("A senha tem que ter no mínimo 1 letra maiúscula")
+        document.getElementById("list_regra4").style.color = '#f33e08f8'
 
+    }else{
+        document.getElementById("list_regra4").style.color = "#48f115f8"
     }
+
     if(temEspeciais == null){
-        erros.push("A senha tem que ter no mínimo 1 símbolo")
+        document.getElementById("list_regra5").style.color = '#f33e08f8'   
+    }else{
+        document.getElementById("list_regra5").style.color = "#48f115f8"
     }
-
-    if(erros.length > 0){
-        alert("A senha deve possuir:\n1 letra minúscula\n2 maiúscula\n1 número\n1 caractere especial")
-        frm.inSenha.value = ''
-        frm.inSenha.focus()
-        return
-    }
-
-    
-    setTimeout(()=>{
-        location.reload()
-    }, 30000)
-
-
-})
+}
